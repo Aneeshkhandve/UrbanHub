ed929f015e7c1e44c2de7bcd4b2dc203da342b2e
Aneeshkhandve <khandveaneesh@gmail.com>
2025-07-24 22:25:02 +0530

    Remove .env file from git tracking

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBBKuj_ADgRNtvx-QtWqAeEF5LLf5dGPL4",
-  authDomain: "urbanhub-cbcf5.firebaseapp.com",
-  projectId: "urbanhub-cbcf5",
-  storageBucket: "urbanhub-cbcf5.firebasestorage.app",
-  messagingSenderId: "204703687631",
-  appId: "1:204703687631:web:3cb8070609103b18fbe8ba"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
